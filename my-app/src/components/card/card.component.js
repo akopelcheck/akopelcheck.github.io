6b8be4482927e54875e1c02c7e9f29f7df372e78
akopelcheck <kopelcheck.1@buckeyemail.osu.edu>
2025-06-25 20:17:59 -0400

    added dark mode functionality

---
+++ my-app/src/components/card/card.component.js
@@ -0,0 +1,10 @@
+import './card.css'
+function Card(){
+    return(
+        <div className='card'>
+            <h2>This is a card!</h2>
+        </div>
+    );
+}
+
+export default Card;
--- my-app/src/pages/home-page/home-page.js
+++ my-app/src/pages/home-page/home-page.js
@@ -1,9 +1,25 @@
 import './home-page.css';
+import Card from '../../components/card/card.component'
+import useLocalStorage from 'use-local-storage'
+
 
 function Home() {
+
+ const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+ const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+
+  const switchTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme)
+  }
+
   return (
-    <div className="Home">
+    <div className="Home" data-theme={theme}>
       <h1>Alexandra Kopelcheck</h1>
+      <Card />
+      <button onClick={switchTheme}>
+        Switch to {theme === 'light' ? 'dark' : 'light'} theme
+      </button>
     </div>
   );
 }
